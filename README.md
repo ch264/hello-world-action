c411f58e06e96091443ac31542c274bd80de0fce
Christina <christinavhastenrath@gmail.com>
2019-12-18 08:18:38 +0100

    added index.js example file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# hello-world-action
+# hello-world-action in JavaScript
+
+I am writin an action from scratch
+
+A readme for a Github Action should contain the following:
+A detailed description of what the action does.
+Required input and output arguments.
+Optional input and output arguments.
+Secrets the action uses.
+Environment variables the action uses.
+An example of how to use your action in a workflow.
+
+## Hello world javascript action
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-action@v1
+with:
+  who-to-greet: 'Mona the Octocat'
---
+++ index.js
@@ -0,0 +1,24 @@
+// GitHub Actions provide context information about the webhook event, Git refs, workflow, action, and the person who triggered the workflow. To access the context information, you can use the github package. The action you'll write will print the webhook event payload the log.
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  
+  const time = (new Date()).toTimeString();
+  
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify
+  
+  (github.context.payload, undefined, 2)
+  
+  console.log(`The event payload: ${payload}`);
+  
+} catch (error) {
+  // If an error is thrown, core.setFailed(error.message); uses the actions toolkit @actions/core package to log a message and set a failing exit code.
+  core.setFailed(error.message);
+}
