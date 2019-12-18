059282211e4f8553e34a804b90cee798478c5f9f
Christina <christinavhastenrath@gmail.com>
2019-12-18 08:49:15 +0100

    backticks in console

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ try {
   
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify
+  // const payload = JSON.stringify
   
-  (github.context.payload, undefined, 2)
+  // (github.context.payload, undefined, 2)
   
   // console.log(`The event payload: ${payload}`);
 
